34f8f1f103a1948ee367433fd0e01fe23aa892c7
Wilson Tamarozzi Junior <wilson.tamarozzi@gmail.com>
2017-01-24 15:50:42 -0200

    Ajuste ortografico :)

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -39,20 +39,20 @@ type buttonCommon struct {
 	buttonAbstract
 }
 
-func NewButtonCommun(text string, sound SoundInterface) *buttonCommon {
+func NewButtonCommon(text string, sound SoundInterface) *buttonCommon {
 	return &buttonCommon{buttonAbstract{text, sound}}
 }
 
 func main() {
-	buttonMP3 := NewButtonCommun("* Botão Comum MP3", &SoundMP3{})
+	buttonMP3 := NewButtonCommon("* Botão Comum MP3", &SoundMP3{})
 	fmt.Println(buttonMP3.GetText())
 	buttonMP3.SoundClick()
 
-	buttonWAV := NewButtonCommun("* Botão Comum WAV", &SoundWAV{})
+	buttonWAV := NewButtonCommon("* Botão Comum WAV", &SoundWAV{})
 	fmt.Println(buttonWAV.GetText())
 	buttonWAV.SoundClick()
 
-	buttonChanged := NewButtonCommun("* Botão Misto", &SoundMP3{})
+	buttonChanged := NewButtonCommon("* Botão Misto", &SoundMP3{})
 	fmt.Println(buttonChanged.GetText())
 	buttonChanged.SoundClick()
 	buttonChanged.ChangeSoundType(&SoundWAV{})
